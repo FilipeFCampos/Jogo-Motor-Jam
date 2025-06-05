c247702541ba881ad906858f1e0cf9aa3f02e822
Adilson <adilsonpsaquino@gmail.com>
2025-06-05 00:36:30 -0300

    por favor

--- Assets/Scripts/BossRoomPortal.cs
+++ Assets/Scripts/BossRoomPortal.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class BossRoomPortal : MonoBehaviour
 {
-    public Transform bossRoomSpawnPoint;
+    public Transform bossSpawnPoint;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = bossRoomSpawnPoint.position;
+            PlayerController inventory = other.GetComponent<PlayerController>();
+            if (inventory != null && inventory.hasKey)
+            {
+                other.transform.position = bossSpawnPoint.position;
+                Debug.Log("Player entrou na sala do boss!");
+            }
+            else
+            {
+                Debug.Log("Você precisa da chave para entrar!");
+            }
         }
     }
 }
